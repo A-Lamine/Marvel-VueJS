609d7276ad75e01ebe14d0d929fb26c2b7df3512
Lamine ADEL <lamine.adel@expaceo.com>
2022-05-23 18:40:02 +0200

    AddStore

--- src/components/characteresCard.vue
+++ src/components/characteresCard.vue
@@ -1,20 +1,39 @@
 <template>
     <div class="charactere">
         <img class="marvel" :src="path + '.' + extension" />
+        <button v-if="store.getters.isfav" v-on:click="click">
+            <ion-icon
+                v-on:click="console.log('hey')"
+                name="heart"
+                size="large"
+                style="color: red"
+            />
+        </button>
+        <button v-else v-on:click="click">
+            <ion-icon name="heart-outline" size="large" style="color: red" />
+        </button>
         <h1>{{ name }}</h1>
     </div>
 </template>
 
 <script setup>
 import { defineProps, toRef } from 'vue'
+import { useStore } from 'vuex'
+
+const store = useStore()
 
 const charactere2 = defineProps({ charactere: Object })
+store.commit('loadStore')
 
 let charactere = toRef(charactere2, 'charactere')
 let {
     name,
     thumbnail: { path, extension },
 } = charactere.value
+
+const click = async () => {
+    store.commit('click')
+}
 </script>
 
 <style scoped>
@@ -32,5 +51,10 @@ h1 {
     width: 100%;
     height: 350px;
     border-radius: 5%;
+}
+button {
+    background-color: white;
+    border: none;
+    cursor: pointer;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,34 @@
 import { createApp } from 'vue'
+import { createStore } from 'vuex'
 import App from './App.vue'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+const store = createStore({
+    state() {
+        return {
+            isfav: false,
+        }
+    },
+    getters: {
+        isfav(state) {
+            return state.isfav
+        },
+    },
+    mutations: {
+        loadStore() {
+            if (localStorage.getItem('store')) {
+                try {
+                    this.replaceState(JSON.parse(localStorage.getItem('store')))
+                } catch (e) {
+                    console.log('Could not initialize store', e)
+                }
+            }
+        },
+        click(state) {
+            console.log('Hey')
+            /* state.isfav ? (state.isfav = false) : (state.isfav = true) */
+        },
+    },
+})
+
+createApp(App).use(router).use(store).mount('#app')
