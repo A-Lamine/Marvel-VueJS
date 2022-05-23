e1e4f0a33b71fb6d7535361437e142f4f06642b6
Lamine ADEL <lamine.adel@expaceo.com>
2022-05-23 10:49:36 +0200

    init

---
+++ src/views/HomeView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <characteresCard msg="Welcome to Your Vue.js App"/>
+      {{ info }}
+  </div>
+</template>
+
+<script setup>
+// @ is an alias to /src
+import characteresCard from '@/components/characteresCard.vue'
+import axios from "axios"
+import { watch } from 'vue';
+
+let myData = {}
+
+
+
+console.log(myData)
+
+
+</script>
