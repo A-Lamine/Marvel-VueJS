c924e9df813bd2870fdaece1c0d69c0e7912558f
Lamine ADEL <lamine.adel@expaceo.com>
2022-05-23 14:13:42 +0200

    GetAllCharacters

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,22 +1,55 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <characteresCard msg="Welcome to Your Vue.js App"/>
-      {{ info }}
-  </div>
+    <div class="characteres">
+        <ul>
+            <li v-for="items in myData" :key="items.id">
+                <p>{{ items.name }}</p>
+            </li>
+        </ul>
+    </div>
 </template>
 
 <script setup>
 // @ is an alias to /src
 import characteresCard from '@/components/characteresCard.vue'
-import axios from "axios"
-import { watch } from 'vue';
+import axios from 'axios'
+import { watch, ref, onBeforeMount } from 'vue'
+import MD5 from 'md5'
 
-let myData = {}
+const { VUE_APP_PUBLIC_KEY, VUE_APP_PRIVATE_KEY, VUE_APP_API_BACKEND } = process.env
+const ts = new Date().getTime()
+const stringToHash = ts + VUE_APP_PRIVATE_KEY + VUE_APP_PUBLIC_KEY
+const hash = MD5(stringToHash).toString()
 
+let myData = ref({})
+let test = [
+    {
+        id: 1,
+        action: 'aze',
+    },
+    {
+        id: 2,
+        action: 'action',
+    },
+]
 
+let page = 0
+const url =
+    VUE_APP_API_BACKEND +
+    '?' +
+    'ts=' +
+    ts +
+    '&apikey=' +
+    VUE_APP_PUBLIC_KEY +
+    '&hash=' +
+    hash +
+    '&limit=99&offset=' +
+    page
 
-console.log(myData)
+watch(myData, (currentValue, oldValue) => {})
 
+const callApi = async () => {
+    await axios.get(url).then(response => (myData.value = response.data.data.results))
+}
 
+onBeforeMount(callApi())
 </script>
