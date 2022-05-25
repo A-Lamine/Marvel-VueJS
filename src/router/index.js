d015f9e6b03c508ce9a6d32730b327263f7bda49
Lamine ADEL <lamine.adel@expaceo.com>
2022-05-25 16:08:03 +0200

    AddDetailViews

--- src/router/index.js
+++ src/router/index.js
@@ -15,6 +15,11 @@ const routes = [
         // which is lazy-loaded when the route is visited.
         component: () => import(/* webpackChunkName: "about" */ '../views/FavoriesView.vue'),
     },
+    {
+        path: '/detail/:id',
+        name: 'detail',
+        component: () => import('../views/DetailsView.vue'),
+    },
 ]
 
 const router = createRouter({
